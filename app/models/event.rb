0251caa8fe86e20d87e9895e9c3b7546d070b759
Gearz <shanegeary@yahoo.com>
2021-04-20 06:44:59 -0600

    added relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     has_one_attatched :poster
+    belongs_to :user 
+    belongs_to :group 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_215141) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
