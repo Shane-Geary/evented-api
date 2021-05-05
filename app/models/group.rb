cf319cefc55a620dc3eba52402cb99f5532b8f9a
Gearz <shanegeary@yahoo.com>
2021-05-05 15:08:31 -0600

    added validations to Groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
     has_many :events 
-end
+
+    validates :name, presence: true, uniqueness: true
+    validates :fav_event, presence: true, uniqueness: true
+    validates :fav_genre, presence: true, uniqueness: true
+    validates :catchphrase, presence: true, uniqueness: true
+end 
